5d5f771428cea0f2cc48cf031705c2acf7b72a29
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-02-07 14:00:19 +0200

    fix: implement bracketsConfig

--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-    while (
-        /\(\)/g.test(str) ||
-        /\[\]/g.test(str) ||
-        /\{\}/g.test(str) ||
-        /\|\|/g.test(str) ||
-        /11/g.test(str) ||
-        /22/g.test(str) ||
-        /33/g.test(str) ||
-        /44/g.test(str) ||
-        /55/g.test(str) ||
-        /66/g.test(str) ||
-        /77/g.test(str) ||
-        /88/g.test(str) ||
-        /99/g.test(str) ||
-        /00/g.test(str)
-    ) {
-        str = str.replace(/\(\)/g, "");
-        str = str.replace(/\[\]/g, "");
-        str = str.replace(/\{\}/g, "");
-        str = str.replace(/\|\|/g, "");
-        str = str.replace(/11/g, "");
-        str = str.replace(/22/g, "");
-        str = str.replace(/33/g, "");
-        str = str.replace(/44/g, "");
-        str = str.replace(/55/g, "");
-        str = str.replace(/66/g, "");
-        str = str.replace(/77/g, "");
-        str = str.replace(/88/g, "");
-        str = str.replace(/99/g, "");
-        str = str.replace(/00/g, "");
-    }
+    let exprConfig = bracketsConfig
+        .reduce((sum, arr) => sum.concat(arr), [])
+        .reduce(
+            (sum, el, index, arr) =>
+                sum +
+                (isNaN(el) ? "\\" : "") +
+                (index % 2 && index !== arr.length - 1 && typeof el !== "number"
+                    ? el + "|"
+                    : el),
+            ""
+        );
 
+    let expr = new RegExp(exprConfig, "g");
+
+    while (expr.test(str)) {
+        str = str.replace(expr, "");
+    }
     return !str.length;
 };
